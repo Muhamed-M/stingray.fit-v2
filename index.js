08df5fbb5e97a6bab5819cf6b8531cfe0d3d5eb0
Muhamed-M <muhamedmuhamedagic03@gmail.com>
2023-02-17 17:18:02 +0100

    predeploy fixes

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 const db = process.env.DB_CLOUD;
 
-// database connection
-mongoose.connect(db, () => console.log('Connected to the database...'));
-
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,4 +21,9 @@ if (process.env.NODE_ENV === 'production') {
     app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname, 'client/dist/index.html')));
 }
 
-app.listen(port, console.log(`Server is running on port ${port}!`));
+// database connection
+mongoose.connect(
+    db,
+    () => console.log('Connected to the database...'),
+    app.listen(port, console.log(`Server is running on port ${port}!`))
+);
