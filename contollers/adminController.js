51ec621ffd4058c146d3edb8b0b5232ae124deeb
Muhamed-M <muhamedmuhamedagic03@gmail.com>
2023-07-09 16:15:00 +0200

    aws s3 added

--- contollers/adminController.js
+++ contollers/adminController.js
@@ -1,8 +1,13 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+require('dotenv').config();
+
+// aws s3 bucket from cyclic.sh
+const BUCKET = process.env.BUCKET;
 
 // models
 const Admin = require('../models/admin');
@@ -11,35 +16,35 @@ const Testimonial = require('../models/testimonials');
 const Tranformation = require('../models/transformations');
 
 // JWT Generator
-const generateToken = id => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
 // @desc   Authenticate admin
 // @route  /api/admin
 // @method post
 const authenticateAdmin = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        // Check for admin email
-        const admin = await Admin.findOne({ email });
+    // Check for admin email
+    const admin = await Admin.findOne({ email });
 
-        if (admin && (await bcrypt.compare(password, admin.password))) {
-            res.json({
-                _id: admin.id,
-                name: admin.name,
-                email: admin.email,
-                token: generateToken(admin._id)
-            });
-        } else {
-            res.status(400).json({
-                message: 'Invalid credentials!'
-            });
-        }
-    } catch (error) {
-        console.log(error);
+    if (admin && (await bcrypt.compare(password, admin.password))) {
+      res.json({
+        _id: admin.id,
+        name: admin.name,
+        email: admin.email,
+        token: generateToken(admin._id),
+      });
+    } else {
+      res.status(400).json({
+        message: 'Invalid credentials!',
+      });
     }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /* <==== Workout plans ====> -CRUD- #START */
@@ -47,31 +52,31 @@ const authenticateAdmin = async (req, res) => {
 // @route  /api/admin/workout-plans
 // @method get
 const getWorkoutPlans = async (req, res) => {
-    try {
-        // find workout plans related data
-        const workoutPlans = await WorkoutPlans.find();
-        res.json(workoutPlans);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    // find workout plans related data
+    const workoutPlans = await WorkoutPlans.find();
+    res.json(workoutPlans);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // @desc   Update Workout Plans price
 // @route  /api/admin/workout-plans/update-price
 // @method put
 const updateWorkoutPlansPrice = async (req, res) => {
-    try {
-        // Gather data
-        const { id } = req.params;
-        const { onlinePrice, personalPrice } = req.body;
-        const price = onlinePrice ? onlinePrice : personalPrice;
+  try {
+    // Gather data
+    const { id } = req.params;
+    const { onlinePrice, personalPrice } = req.body;
+    const price = onlinePrice ? onlinePrice : personalPrice;
 
-        // update price
-        await WorkoutPlans.findByIdAndUpdate(id, { price: price });
-        res.status(200).json({ message: 'Price updated successfully!' });
-    } catch (error) {
-        console.log(error);
-    }
+    // update price
+    await WorkoutPlans.findByIdAndUpdate(id, { price: price });
+    res.status(200).json({ message: 'Price updated successfully!' });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /* <==== Testimonials ====> -CRUD- #START */
@@ -79,46 +84,46 @@ const updateWorkoutPlansPrice = async (req, res) => {
 // @route  /api/admin/testimonials
 // @method post
 const createTestimonial = async (req, res) => {
-    try {
-        const { fullname, profession, text } = req.body;
+  try {
+    const { fullname, profession, text } = req.body;
 
-        // Create testimonial
-        await Testimonial.create({
-            fullname,
-            profession,
-            text
-        });
+    // Create testimonial
+    await Testimonial.create({
+      fullname,
+      profession,
+      text,
+    });
 
-        res.status(200).json({ message: 'Testimonial created successfully!' });
-    } catch (error) {
-        console.log(error);
-        res.json({ message: 'Testimonial creation failed!' });
-    }
+    res.status(200).json({ message: 'Testimonial created successfully!' });
+  } catch (error) {
+    console.log(error);
+    res.json({ message: 'Testimonial creation failed!' });
+  }
 };
 
 // @desc   Get all testimonials
 // @route  /api/admin/testimonials
 // @method get
 const getTestimonials = async (req, res) => {
-    try {
-        const testimonials = await Testimonial.find();
-        res.json(testimonials);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const testimonials = await Testimonial.find();
+    res.json(testimonials);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // @desc   Delete testimonial
 // @route  /api/admin/testimonials/:id
 // @method delete
 const deleteTestimonial = async (req, res) => {
-    try {
-        const { id } = req.params;
-        await Testimonial.deleteOne({ _id: id });
-        res.status(200).json({ message: 'Testimonial deleted successfully!' });
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const { id } = req.params;
+    await Testimonial.deleteOne({ _id: id });
+    res.status(200).json({ message: 'Testimonial deleted successfully!' });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /* <==== Transformations ====> -CRUD- #START */
@@ -126,75 +131,131 @@ const deleteTestimonial = async (req, res) => {
 // @route  /api/admin/transformations
 // @method post
 const uploadTransformation = async (req, res) => {
-    const { buffer, originalname } = req.file;
-    const filename = Date.now() + path.extname(originalname);
+  const { buffer, originalname } = req.file;
+  const filename = Date.now() + path.extname(originalname);
+  // store image and thumbnail paths
+  // const imagePath = `./public/images/${filename}`;
+  // const thumbnailPath = `./public/thumbnails/${filename}`;
+
+  // let transformation;
+  try {
+    // optimize image
+    //   sharp(buffer).jpeg({ mozjpeg: true, quality: 30 }).toFile(imagePath);
+
+    // optimize image for thumbnails
+    //   sharp(buffer)
+    //     .resize(280, 200)
+    //     .jpeg({ mozjpeg: true, quality: 30 })
+    //     .toFile(thumbnailPath);
+
+    // optimize image
+    const optimizedImage = await sharp(buffer)
+      .jpeg({ mozjpeg: true, quality: 30 })
+      .toBuffer();
+
+    // optimize image for thumbnails
+    const thumbnailImage = await sharp(buffer)
+      .resize(280, 200)
+      .jpeg({ mozjpeg: true, quality: 30 })
+      .toBuffer();
+
+    const params1 = {
+      Bucket: BUCKET,
+      Key: `images/${filename}`,
+      Body: optimizedImage,
+    };
+
+    const params2 = {
+      Bucket: BUCKET,
+      Key: `thumbnails/${filename}`,
+      Body: thumbnailImage,
+    };
+
+    // Call S3 to retrieve upload file to specified bucket
+    const uploadPromise1 = s3.upload(params1).promise();
+    const uploadPromise2 = s3.upload(params2).promise();
+
+    const [upload1, upload2] = await Promise.all([
+      uploadPromise1,
+      uploadPromise2,
+    ]);
+
     // store image and thumbnail paths
-    const imagePath = `./public/images/${filename}`;
-    const thumbnailPath = `./public/thumbnails/${filename}`;
+    const imagePath = upload1.Location;
+    const thumbnailPath = upload2.Location;
 
-    let transformation;
-    try {
-        // Store file path to db
-        transformation = await Tranformation.create({
-            imagePath,
-            thumbnailPath
-        });
+    // Store file path to db
+    await Tranformation.create({
+      imagePath,
+      thumbnailPath,
+    });
 
-        // optimize image
-        sharp(buffer).jpeg({ mozjpeg: true, quality: 30 }).toFile(imagePath);
-
-        // optimize image for thumbnails
-        sharp(buffer).resize(280, 200).jpeg({ mozjpeg: true, quality: 30 }).toFile(thumbnailPath);
-        res.status(200).json({ message: 'Transformation uploaded successfully!' });
-    } catch (error) {
-        fs.unlink(transformation.imagePath, error => {
-            if (error) console.error(error);
-        });
-        res.json({ message: error });
-        console.log(error);
-    }
+    res.status(200).json({ message: 'Transformation uploaded successfully!' });
+  } catch (error) {
+    // fs.unlink(transformation.imagePath, (error) => {
+    //   if (error) console.error(error);
+    // });
+    res.status(500).json({ message: error.message });
+    console.log(error);
+  }
 };
 
 // @desc   Get all transformations
 // @route  /api/admin/transformations
 // @method get
 const getTransformations = async (req, res) => {
-    try {
-        const transformations = await Tranformation.find();
-        res.json(transformations);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const transformations = await Tranformation.find();
+    res.json(transformations);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // @desc   Delete transformation
 // @route  /api/admin/transformation/:id
 // @method delete
 const deleteTransformation = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { imagePath, thumbnailPath } = req.body;
+  const { id } = req.params;
+  const { imagePath, thumbnailPath } = req.body;
+  console.log(imagePath);
 
-        await Tranformation.deleteOne({ _id: id });
-        // delete image from files
-        const files = [imagePath, thumbnailPath];
-        files.forEach(file => {
-            fs.unlinkSync(file);
-        });
-        res.status(200).json({ message: 'Transformation deleted successfully!' });
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    // delete transformation from db
+    await Tranformation.deleteOne({ _id: id });
+
+    // create parameters for delete call
+    const params1 = {
+      Bucket: BUCKET,
+      Key: imagePath.replace(`https://${BUCKET}.s3.amazonaws.com/`, ''),
+    };
+
+    const params2 = {
+      Bucket: BUCKET,
+      Key: thumbnailPath.replace(`https://${BUCKET}.s3.amazonaws.com/`, ''),
+    };
+
+    // delete images from s3 bucket
+    const deletePromise1 = s3.deleteObject(params1).promise();
+    const deletePromise2 = s3.deleteObject(params2).promise();
+
+    await Promise.all([deletePromise1, deletePromise2]);
+
+    res.status(200).json({ message: 'Transformation deleted successfully!' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = {
-    authenticateAdmin,
-    getWorkoutPlans,
-    updateWorkoutPlansPrice,
-    createTestimonial,
-    getTestimonials,
-    deleteTestimonial,
-    uploadTransformation,
-    getTransformations,
-    deleteTransformation
+  authenticateAdmin,
+  getWorkoutPlans,
+  updateWorkoutPlansPrice,
+  createTestimonial,
+  getTestimonials,
+  deleteTestimonial,
+  uploadTransformation,
+  getTransformations,
+  deleteTransformation,
 };
