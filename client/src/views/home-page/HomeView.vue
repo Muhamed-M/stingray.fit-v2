5e26db50eedbd3e9ab89cd6ac2dd85d1fa4ccac1
Muhamed-M <muhamedmuhamedagic03@gmail.com>
2023-07-24 10:12:17 +0200

    lazy loading

--- client/src/views/home-page/HomeView.vue
+++ client/src/views/home-page/HomeView.vue
@@ -1,14 +1,40 @@
 <script setup>
+import { defineAsyncComponent } from 'vue';
+
 import NavComponent from '@/components/NavComponent.vue';
 import HeroSection from '@/components/HeroSection.vue';
-import CardsSection from '@/components/CardsSection.vue';
-import AboutSection from '@/components/AboutSection.vue';
-import WhatsIncluded from '@/components/WhatsIncluded.vue';
-import VideosSection from '@/components/VideosSection.vue';
-import ShopSection from '@/components/ShopSection.vue';
-import TransformationsSection from '@/components/TransformationsSection.vue';
-import TestimonialsSection from '@/components/TestimonialsSection.vue';
-import FooterSection from '@/components/FooterSection.vue';
+const CardsSection = defineAsyncComponent(() =>
+  import('@/components/CardsSection.vue')
+);
+const AboutSection = defineAsyncComponent(() =>
+  import('@/components/AboutSection.vue')
+);
+const WhatsIncluded = defineAsyncComponent(() =>
+  import('@/components/WhatsIncluded.vue')
+);
+const VideosSection = defineAsyncComponent(() =>
+  import('@/components/VideosSection.vue')
+);
+const ShopSection = defineAsyncComponent(() =>
+  import('@/components/ShopSection.vue')
+);
+const TransformationsSection = defineAsyncComponent(() =>
+  import('@/components/TransformationsSection.vue')
+);
+const TestimonialsSection = defineAsyncComponent(() =>
+  import('@/components/TestimonialsSection.vue')
+);
+const FooterSection = defineAsyncComponent(() =>
+  import('@/components/FooterSection.vue')
+);
+// import CardsSection from '@/components/CardsSection.vue';
+// import AboutSection from '@/components/AboutSection.vue';
+// import WhatsIncluded from '@/components/WhatsIncluded.vue';
+// import VideosSection from '@/components/VideosSection.vue';
+// import ShopSection from '@/components/ShopSection.vue';
+// import TransformationsSection from '@/components/TransformationsSection.vue';
+// import TestimonialsSection from '@/components/TestimonialsSection.vue';
+// import FooterSection from '@/components/FooterSection.vue';
 </script>
 
 <template>
